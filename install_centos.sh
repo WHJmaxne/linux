d95de0dfe9a068dd0509ddba8aa88f3d1b435a1b
whj <whjmaxne@outlook.com>
2019-10-18 09:51:37 +0800

    none

--- install_centos.sh
+++ install_centos.sh
@@ -177,12 +177,11 @@ install_ngrok(){
   cd /opt/ngrok
   read -p "请输入域名：" domain
   cat > build.sh <<-EOF
-export NGROK_DOMAIN="$domain"
 cd /ngrok/
 openssl genrsa -out rootCA.key 2048
-openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
+openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$domain" -days 5000 -out rootCA.pem
 openssl genrsa -out device.key 2048
-openssl req -new -key device.key -subj "/CN=$NGROK_DOMAIN" -out device.csr
+openssl req -new -key device.key -subj "/CN=$domain" -out device.csr
 openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
 cp rootCA.pem assets/client/tls/ngrokroot.crt
 cp device.crt assets/server/tls/snakeoil.crt
